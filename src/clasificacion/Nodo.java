45bff3fa44510fd9bd70684d4e948f00ef41040e
Tito Jaramillo <titoj740@gmail.com>
2023-07-02 11:57:12 -0500

    Commit inicial

---
+++ src/clasificacion/Nodo.java
@@ -0,0 +1,13 @@
+package clasificacion;
+
+import java.util.ArrayList;
+import java.util.SortedSet;
+
+public class Nodo {
+    private String respuesta;
+    private String pregunta;
+    private ArrayList<Nodo> hijos;
+
+    public Nodo(String respuesta, String pregunta, ArrayList<Nodo> hijos) {
+    }
+}
