8992f2b4803db47d497d92dc5afd77424cab78b0
Tito Jaramillo <titoj740@gmail.com>
2023-07-02 20:23:25 -0500

    Árbol de desición funcional

--- src/clasificacion/Nodo.java
+++ src/clasificacion/Nodo.java
@@ -4,10 +4,55 @@ import java.util.ArrayList;
 import java.util.SortedSet;
 
 public class Nodo {
-    private String respuesta;
+    private Object respuesta;
     private String pregunta;
     private ArrayList<Nodo> hijos;
 
-    public Nodo(String respuesta, String pregunta, ArrayList<Nodo> hijos) {
+    public Nodo(Object respuesta, String pregunta) {
+        this.respuesta = respuesta;
+        this.pregunta = pregunta;
+        this.hijos = new ArrayList<>();
+    }
+
+    public Object getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(Object respuesta) {
+        this.respuesta = respuesta;
+    }
+
+    public String getPregunta() {
+        return pregunta;
+    }
+
+    public void setPregunta(String pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    public ArrayList<Nodo> getHijos() {
+        return hijos;
+    }
+
+    public void setHijos(ArrayList<Nodo> hijos) {
+        this.hijos = hijos;
+    }
+
+    public boolean agregarHijo(Nodo hijo){
+        for(Nodo n: hijos){
+            if (n.getRespuesta().equals(hijo.respuesta)){
+                return false;
+            }
+        }
+        hijos.add(hijo);
+        return true;
+    }
+
+    public Nodo buscarHijo(Object buscado){
+        for (Nodo n: hijos){
+            if (buscado.equals(n.respuesta))
+                return n;
+        }
+        return null;
     }
 }
